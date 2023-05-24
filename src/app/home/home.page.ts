ce35859a2d6040e77efcce87936fdb97340eed23
Silvio bis <primoboby@hotmail.com>
2023-05-23 22:32:32 -0300

    Subindo

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,71 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Geolocation, Position } from '@capacitor/geolocation';
+import { GoogleMap } from '@capacitor/google-maps';
+import { environment } from 'src/environments/environment';
+
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  @ViewChild('map')  mapRef!: ElementRef<HTMLElement>;
+  map!: GoogleMap;
+
+  constructor() {}
+
+  ionViewWillEnter(){
+    this.createMap()
+  }
+
+  async createMap() {
+    this.map = await GoogleMap.create({
+      id: 'my-map',
+      element: this.mapRef.nativeElement,
+      apiKey: environment.mapsKey,
+      config: {
+        center: {
+          lat: -22.489418,
+          lng: -48.546515,
+        },
+        zoom: 8,
+      },
+    });
+
+    this.buscarPosicao();
+  }
+
+  async  buscarPosicao(){
+    const coordinates = await Geolocation.getCurrentPosition();
+
+  console.log('Current position:', coordinates);
+    this.adicionarMarcador(coordinates);
+    return coordinates;
+  }
+
+  async adicionarMarcador(coordinates: Position){
+    // Add a marker to the map
+    const markerId = await this.map.addMarker({
+      coordinate: {
+        lat: coordinates.coords.latitude,
+        lng: coordinates.coords.longitude
+      }
+    });
+    this.zoomMarcador(coordinates);
+  }
+
+  zoomMarcador(coordinates: Position){
+    this.map.setCamera({
+      coordinate:{
+        lat: coordinates.coords.latitude,
+        lng: coordinates.coords.longitude
+      },
+      zoom: 15,
+      animate: true
+    });
+  }
+
+
+}
